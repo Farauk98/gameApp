f2d5d03cd55fea9b6ed56ad73fd167a973c5dd87
Rafal <kornat.rafal@gmail.com>
2022-03-17 21:57:51 +0100

    first view from tutorial

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'battleship.apps.BattleshipConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
